0188025755bfea377a85e79fb7e9dfd7fc2a1053
rachmanj <rachmanj@gmail.com>
2023-08-16 15:11:31 +0800

    First commit

---
+++ src/app/product/[id]/page.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+
+import { singleProduct } from "@/data";
+import Price from "@/components/Price";
+
+const SingleProductPage = () => {
+  return (
+    <div className="p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-500 md:flex-row md:gap-8 md:items-center">
+      {/* IMAGE CONTAINER */}
+      {singleProduct.img && (
+        <div className="relative w-full h-1/2 md:[70%]">
+          <Image
+            src={singleProduct.img}
+            alt=""
+            className="object-contain"
+            fill
+          />
+        </div>
+      )}
+
+      {/* TEXT CONTAINER */}
+      <div className="h-1/2 flex flex-col gap-4 md:[70%] md:justify-center md:gap-6 xl:gap-8">
+        <h1 className="text-3xl font-bold uppercase">{singleProduct.title}</h1>
+        <p>{singleProduct.desc}</p>
+        <Price
+          price={singleProduct.price}
+          id={singleProduct.id}
+          options={singleProduct.options}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SingleProductPage;
